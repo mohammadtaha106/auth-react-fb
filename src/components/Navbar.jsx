7ef7c1512295ae7c84862f57cf0e67a2f94fdbc2
mohammadtaha106 <mohammadtaha19888@gmail.com>
2024-09-10 16:05:37 -0700

    h

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
         setTimeout(() => {
           navigate("/signin");
-        }, 2000);
+        }, 1000);
       })
       .catch((error) => {
         console.log(error);
